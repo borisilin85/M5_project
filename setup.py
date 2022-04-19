c3f0237211977ebe45de143dd556fb7cd0b479e0
borisilin85 <borisilin85@gmail.com>
2022-04-19 15:48:44 +0300

    yml file

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ setuptools.setup(name="M5 project",
                  author='Boris',
                  version="1.0.0",
                  long_description=long_description,
-                 scripts = ['m5_initial_nn_model.py'],
+                 scripts = ['src/M5_project/m5_initial_nn_model.py'],
                  description="Initial NN model for M5 project",
                  packages=setuptools.find_packages(),
                  python_requires='>=3.7',
                  project_urls={
                      "Git url": "https://github.com/borisilin85/M5_project",
 
-                 }
+                 },
+                 install_requires=['pytest']
+
                  )
